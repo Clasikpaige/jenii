8e8da1a1cb6d7f6a65b5b603d1e3d8e445e24ed3
Paige <clasikpaige@gmail.com>
2023-03-11 01:59:50 +0000

    Update header.js

--- public/component/header.js
+++ public/component/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+[9:12 am, 11/03/2023] PAiGE: import React from 'react';
 import { StyleSheet, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 const Header = () => {
@@ -53,6 +53,65 @@ const styles = StyleSheet.create({
   profileIcon: {
     width: 25,
     height: 25,
+  },
+});
+
+export default Header;
+[9:58 am, 11/03/2023] PAiGE: import React from 'react';
+import { View, Image, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; // Assuming you're using Expo
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Image style={styles.logo} source={require('../assets/logo.png')} />
+      <View style={styles.searchContainer}>
+        <MaterialIcons name="search" size={24} color="black" />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search for food or restaurant"
+        />
+      </View>
+      <TouchableOpacity>
+        <Image style={styles.profile} source={require('../assets/profile.png')} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  logo: {
+    width: 100,
+    height: 40,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 20,
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  profile: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
   },
 });
 
